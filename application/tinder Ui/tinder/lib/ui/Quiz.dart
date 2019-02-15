d949c40e3135c8548b0ccbc4e9ced69612f0430d
Aritra <aritra.born2fly@gmail.com>
2019-02-15 23:18:47 +0530

    tinder fixed BOHOT HARD

--- application/tinder Ui/tinder/lib/ui/Quiz.dart
+++ application/tinder Ui/tinder/lib/ui/Quiz.dart
@@ -18,71 +18,60 @@ class Quiz extends StatelessWidget {
         ),
         // this code is used to get the gradient
 
-        child: new Center(
-          child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              new Padding(
-                padding: EdgeInsets.all(15.0),
-              ),
-              new Image.asset("assets/cuboid.png",
-                  height: (MediaQuery.of(context).size.height) * 0.55),
-              new Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  new Padding(
-                    padding: EdgeInsets.all(20.0),
-                  ),
-                  new Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      new GestureDetector(
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(
-                              builder: (BuildContext context) {
-                            return new Answer(1);
-                          }));
-                        },
-                        child: new Hero(
-                            tag: 'hero1',
-                            child: new CircleAvatar(
-                              child: new Icon(
-                                Icons.check,
-                                size: MediaQuery.of(context).size.width * 0.25,
-                              ),
-                              maxRadius:  MediaQuery.of(context).size.width * 0.15,
-                              backgroundColor: Colors.blue[100],
-                            )),
-                      ),
-                      new Padding(
-                        padding: EdgeInsets.all(5.0),
-                      ),
-                      new GestureDetector(
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(
-                              builder: (BuildContext context) {
-                            return new Answer(2);
-                          }));
-                        },
-                        child: new Hero(
-                            tag: 'hero2',
-                            child: new CircleAvatar(
-                              child: new Icon(
-                                Icons.clear,
-                                size: MediaQuery.of(context).size.width * 0.25,
-                              ),
-                              maxRadius: MediaQuery.of(context).size.width * 0.15,
-                              
-                              backgroundColor: Colors.blue[100],
-                            )),
-                      ),
-                    ],
-                  ),
-                ],
-              )
-            ],
-          ),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            new Image.asset("assets/cuboid.png",
+                height: (MediaQuery.of(context).size.height) * 0.45),
+
+
+            new Row(
+              
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                new GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (BuildContext context) {
+                      return new Answer(1);
+                    }));
+                  },
+                  child: new Hero(
+                      tag: 'hero1',
+                      child: new CircleAvatar(
+                        child: new Icon(
+                          Icons.check,
+                          size: MediaQuery.of(context).size.width * 0.20,
+                        ),
+                        maxRadius: MediaQuery.of(context).size.width * 0.10,
+                        backgroundColor: Colors.blue[100],
+                      )),
+                ),
+                new Padding(
+                  padding: EdgeInsets.all(5.0),
+                ),
+                new GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (BuildContext context) {
+                      return new Answer(2);
+                    }));
+                  },
+                  child: new Hero(
+                      tag: 'hero2',
+                      child: new CircleAvatar(
+                        child: new Icon(
+                          Icons.clear,
+                          size: MediaQuery.of(context).size.width * 0.20,
+                        ),
+                        maxRadius: MediaQuery.of(context).size.width * 0.10,
+                        backgroundColor: Colors.blue[100],
+                      )),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
