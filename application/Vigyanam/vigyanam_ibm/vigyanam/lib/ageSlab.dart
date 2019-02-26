53b533e7c2e46a8a9a79b31b12cc0bbb757d9f2c
Aritra <aritra.born2fly@gmail.com>
2019-02-26 15:01:28 +0530

    Vigyanam IBM

---
+++ application/Vigyanam/vigyanam_ibm/vigyanam/lib/ageSlab.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'main.dart';
+import 'animateAge.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class Age extends StatefulWidget {
+  @override
+  _AgeState createState() => _AgeState();
+}
+
+class _AgeState extends State<Age> {
+  Location location = new Location();
+  bool recieveLoc = false;
+  
+  @override
+  void initState() {
+    super.initState();
+    //default variables set is 0
+    currentLocation['latitude'] = 0.0;
+    currentLocation['longitude'] = 0.0;
+
+    initPlatformState();
+  }
+
+  void initPlatformState() async {
+    location.getLocation().then((myLocation) {
+      setState(() {
+        recieveLoc = true;
+        currentLocation = myLocation;
+        auth['latitude'] = currentLocation['latitude'];
+        auth['longitude'] = currentLocation['longitude'];
+      });
+    });
+  }
+
+//the function that is used to make the age slab
+  GestureDetector func(var a) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (BuildContext context) {
+          return new AnimateAge(a);
+        }));
+      },
+      child: new Hero(
+        tag: 'hero$a',
+        child: CachedNetworkImage(
+            imageUrl:
+                "https://sih-temper-app.eu-gb.mybluemix.net/assetrepo/age_slab/age_${a}.png",
+            placeholder: (context, url) => new CircularProgressIndicator(),
+            errorWidget: (context, url, error) => new Icon(Icons.error),
+            width: (MediaQuery.of(context).size.width) * 0.40,
+            height: (MediaQuery.of(context).size.height) * 0.20),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //if the location is recieved
+    if (recieveLoc) {
+      return Scaffold(
+        //for Testing
+        // body: new Center(
+        //   child: new Text(
+        //       "${currentLocation['longitude']}\n${currentLocation['latitude']}"),
+        // ),
+        //for Testing
+        body: new Center(
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  new Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      func(1),
+                      func(2),
+                    ],
+                  ),
+                  new Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      func(3),
+                      func(4),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+    }
+    //if the location is not recieved
+    else {
+      return Scaffold(
+        body: new Center(
+          child: new Text("^_^"),
+        ),
+      );
+    }
+  }
+}
---
+++ application/Vigyanam/vigyanam_ibm/vigyanam/lib/animateAge.dart
@@ -0,0 +1,104 @@
+import 'dart:convert';
+import 'quizMain.dart';
+import 'main.dart';
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:http/http.dart' as http;
+import 'errorPage.dart';
+
+//error message
+String error = "";
+//error message
+
+class AnimateAge extends StatefulWidget {
+  final int a;
+  AnimateAge(this.a);
+  @override
+  _AnimateAgeState createState() => _AnimateAgeState(a);
+}
+
+class _AnimateAgeState extends State<AnimateAge> {
+  bool recieveAuth = false;
+
+  final int a;
+  _AnimateAgeState(this.a);
+
+  @override
+  initState() {
+    super.initState();
+    auth['age_slab'] = a;
+    sendAuth().then((_) {
+      print("Sent and recieved");
+      setState(() {
+        recieveAuth = true;
+      });
+    });
+  }
+
+  //function that makes the animation
+  WillPopScope fun(var index) {
+    return new WillPopScope(
+      onWillPop: () async => false,
+      child: CachedNetworkImage(
+          imageUrl:
+              "https://sih-temper-app.eu-gb.mybluemix.net/assetrepo/age_slab/age_${index}.png",
+          placeholder: (context, url) => new CircularProgressIndicator(),
+          errorWidget: (context, url, error) => new Icon(Icons.error),
+          height: (MediaQuery.of(context).size.height) * 0.70,
+          width: (MediaQuery.of(context).size.width) * 0.70),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Container(
+        child: new Center(
+          child: new Hero(
+            tag: "hero$a",
+            child: fun(a),
+          ),
+        ),
+      ),
+      floatingActionButton: recieveAuth
+          ? new FloatingActionButton(
+              onPressed: () {
+                print("I have Recieved it");
+                print("Error is $error");
+                if (error == "201") {
+                  //QUIZ page
+                  print("Quiz");
+                  Navigator.push(context, new MaterialPageRoute(builder: (context)=>quizMain()));
+                } else {
+                  //ERROR page
+                  print("Fuck Off");
+                  Navigator.push(context, new MaterialPageRoute(builder: (context)=>ErrorPage()));
+                }
+              },
+              child: Icon(Icons.arrow_forward),
+              foregroundColor: Color(0xFF128fff),
+              backgroundColor: Color(0xFFffffff),
+            )
+          : new FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.cached),
+              foregroundColor: Color(0xFFffffff),
+              backgroundColor: Colors.red,
+            ),
+    );
+  }
+}
+
+Future<void> sendAuth() async {
+  print(auth);
+  var s = await http.put(
+      Uri.parse(
+          "https://sih-temper-app.eu-gb.mybluemix.net/sendusrloc/${auth['usr_id']}"),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(auth),
+      encoding: Encoding.getByName("utf-8"));
+  Map l = json.decode(s.body);
+  error = l['code'];
+  print("error is $error");
+  print(l);
+}
